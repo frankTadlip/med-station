7451d8066410da749443dce98edb53c7e4290767
Frank Tadlip <frank@bizbox.ph>
2017-07-15 21:38:44 +0800

    main page shell

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import Radium, { StyleRoot } from 'radium';
 
-const Layout = ({content = () => null }) => (
-  <div>
-    <div>
-      {content()}
-    </div>
-  </div>
-);
+
+import Header from './header';
+
+@Radium
+class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <StyleRoot>
+                <main>
+                    <Header />
+                </main>
+            </StyleRoot>
+        ) 
+    }
+}
 
 export default Layout;
