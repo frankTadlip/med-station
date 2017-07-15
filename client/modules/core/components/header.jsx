2efde6d91a4a9ba066f76eb761e114056d06b630
Frank Tadlip <frank@bizbox.ph>
2017-07-15 21:26:41 +0800

    header file

---
+++ client/modules/core/components/header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Radium, { StyleRoot } from 'radium';
+
+// material
+import AppBar from 'material-ui/AppBar';
+import MoreVert from 'material-ui/svg-icons/navigation/more-vert';
+
+const styles = {
+    appbar: {
+        '@media (min-width: 768px)': {
+            paddingLeft: '256px'
+        }
+    },
+};
+
+@Radium
+class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <StyleRoot>
+                <header>
+                    <AppBar
+                        title="Title"
+                    />
+                </header>
+            </StyleRoot>
+        );
+    }
+}
+
+export default Header;
